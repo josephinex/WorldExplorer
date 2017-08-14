aca369bfd9ce76d21fe714d111d32bcdd81708c1
josephinex <josephinex21@gmail.com>
2017-08-14 13:46:15 +0300

    Replace Autowired by Managedproperty

--- src/app/beans/CitiesByCountryNameBean.java
+++ src/app/beans/CitiesByCountryNameBean.java
@@ -1,16 +1,30 @@
 package app.beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
-import app.dao.CityRepositoryImpl;
+import app.entities.City;
+import app.repositories.CityRepository;
 
 @ManagedBean(name = "cities_by_countryname", eager = true)
 @RequestScoped
 public class CitiesByCountryNameBean implements Serializable {
+
+	@ManagedProperty(value = "#{CityRepository}")
+	CityRepository cityRepository;
+
+	public CityRepository getCityRepository() {
+		return cityRepository;
+	}
+
+	public void setCityRepository(CityRepository cityRepository) {
+		this.cityRepository = cityRepository;
+	}
 
 	private String countryName = "";
 	private List<String> cityNames = null;
@@ -32,10 +46,15 @@ public class CitiesByCountryNameBean implements Serializable {
 	}
 
 	public void submit() {
+		List<City> cities = cityRepository.findByCountryName(getCountryName());
 
-		CityRepositoryImpl cityRepo = new CityRepositoryImpl();
-		cityRepo.findCities();
+		List<String> cityNames = new ArrayList<>();
 
+		for (City city : cities) {
+			cityNames.add(city.getCityName());
+		}
+
+		setCityNames(cityNames);
 	}
 
 }
