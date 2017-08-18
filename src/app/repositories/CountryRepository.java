04a8ac0c3db67e1a211ce225cf07c709a53f608e
josephinex <josephinex21@gmail.com>
2017-08-18 10:11:37 +0300

    Add new method and query. Refactory getting country names by country code

--- src/app/repositories/CountryRepository.java
+++ src/app/repositories/CountryRepository.java
@@ -13,4 +13,7 @@ public interface CountryRepository extends ReadOnlyRepository<Country, Long> {
 	
 	@Query("select co from Country co join co.cities c where c.cityName = :cityName")
 	public List<Country> findByCityName(@Param("cityName")String cityName);
+	
+	@Query("select c from Country c where c.countryCode = :countryCode")
+	public List<Country> findByCountryCode(@Param("countryCode")String countryCode);
 }
