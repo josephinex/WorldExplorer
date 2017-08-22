e771c9019f0263b3e1e581f092e5d4ce6e588166
josephinex <josephinex21@gmail.com>
2017-08-22 22:48:32 +0300

    Map entity to dto

--- src/app/service/CityRegistrationService.java
+++ src/app/service/CityRegistrationService.java
@@ -1,24 +1,41 @@
 package app.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
 import app.dto.CityDto;
+import app.entities.City;
 import app.repositories.CityRepository;
 
-@Service(value="CityRegistrationService")
+@Service(value = "CityRegistrationService")
 public class CityRegistrationService {
-	
+
 	@Autowired
 	private CityRepository cityRepository;
 
-	public CityRepository getCityRepository() {
-		return cityRepository;
+	public List<CityDto> findByCountryName(String countryName) {
+		List<City> originalList = cityRepository.findByCountryName(countryName);
+		List<CityDto> list = new ArrayList<>(originalList.size());
+
+		// mapping from city to citydto
+
+		for (City city : originalList) {
+			CityDto cityDto = new CityDto();
+			cityDto.setCityName(city.getCityName());
+			cityDto.setCountry(city.getCountry());
+			cityDto.setCountryCode(city.getCountryCode());
+			list.add(cityDto);
+		}
+
+		return list;
 	}
 
 	public void setCityRepository(CityRepository cityRepository) {
 		this.cityRepository = cityRepository;
 	}
-	
+
 }
